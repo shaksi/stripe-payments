5de513ce4e0cd8d978d7f32cf13e6bce6c9d8b13
Shaksi <iam@shak.si>
2018-05-26 18:30:16 +0100

    update copy.

--- server/orders.js
+++ server/orders.js
@@ -80,7 +80,7 @@ const twilioMessage = (name, number) => {
     body:
       'Hi ' +
       name +
-      ', this is to confirm we have received your order for an IQOS trial. We will do some age verification and let you know when your package is on its way. If you have any questions or want to get in touch call xxxx or email me at xxxx - Thanks, Emma at IQOS support',
+      ', this is to confirm we have received your order for an IQOS trial. We will do some age verification and let you know when your package is on its way. If you have any questions or want to get in touch call/WhatsApp 07860909992 or email me at tryIQOS.uk@pmi.com - Thanks, Emma',
   });
 };
 exports.orders = {
